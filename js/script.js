5d217bcc8e6b84e697adae9e516f8b2f7b87d356
LautaroSF <lautaroproofthebro@gmail.com>
2026-07-09 17:37:07 -0300

    Update script.js
    Segundo armado del script js, añadí la función del cambio de fondo y letras a otro tipo de color cuando se presiona el botón indicado (específicamente el que esta ubicado en la parte de las obras junto con sus nombres y año respectivos).

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
+// Selecciono la ID del boton correspondiente.
+let bt2 = document.querySelector('#btn-tema');
+let temaActual = 1; // Creo una variable para el color actual de la pagina.
+
+bt2.addEventListener('click', function() {
+
+    if (temaActual === 1) {
+		// Tema de fondo oscuro y letras blancas.
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+        
+        temaActual = 2;
+
+    } else if (temaActual === 2) {
+        // Tema de fondo blanco y letras negras.
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+        
+        temaActual = 3;
+
+    } else if (temaActual === 3) {
+        // Tema de fondo azul y letras amarillas.
+        document.body.style.backgroundColor = "blue";
+        document.body.style.color = "yellow";
+        
+        temaActual = 1;   // Vuelve al primero
+    }
+
+});
+
 // Hago un array de objetos sobre las diferentes obras.
 // Pongo el titulo correspondiente y el src de donde sacaran la imagen.
 let obras = [
